2178cb88d71e856c5e86b99f957563d3c46137fd
Balint Tobik <balint.tobik@nokia.com>
2020-04-21 15:20:30 +0200

    Handle only local StorageClass PVCs and PVs

--- pkg/k8sclient/k8sclient.go
+++ pkg/k8sclient/k8sclient.go
@@ -1,8 +1,8 @@
 package k8sclient
 
 import (
-	"log"
 	"errors"
+	"context"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -13,6 +13,8 @@ const (
 	NodeSelector = "nokia.k8s.io/nodeSelector"
 	NodeName = "nokia.k8s.io/nodeName"
 	LvCapacity = "nokia.k8s.io/lv-capacity"
+	LocalScProvisioner = "nokia.k8s.io/local"
+	LocalAnnotation = "volume.beta.kubernetes.io/storage-provisioner"
 )
 
 func GetNodeByLabel(label string, kubeClient kubernetes.Interface) (v1.Node, error) {
@@ -20,11 +22,8 @@ func GetNodeByLabel(label string, kubeClient kubernetes.Interface) (v1.Node, err
 	var maxCapacity int64 = 0
 	var listOption metav1.ListOptions
 
-	// if label != "" {
 	listOption = metav1.ListOptions{LabelSelector: label}
-	// }
-	nodeList, err := kubeClient.CoreV1().Nodes().List(listOption)
-	log.Printf("DEBUG: result node list: %+v\n", nodeList)
+	nodeList, err := kubeClient.CoreV1().Nodes().List(context.TODO(), listOption)
 	if err != nil {
 		return v1.Node{}, err
 	}
@@ -49,17 +48,15 @@ func GetNodeByLabel(label string, kubeClient kubernetes.Interface) (v1.Node, err
 }
 
 func UpdatePvc(pvc v1.PersistentVolumeClaim, kubeClient kubernetes.Interface) error {
-	log.Printf("DEBUG: UpdatePvc PVC for update: %+v\n", pvc)
-	result, err := kubeClient.CoreV1().PersistentVolumeClaims(pvc.ObjectMeta.Namespace).Update(&pvc)
+	_, err := kubeClient.CoreV1().PersistentVolumeClaims(pvc.ObjectMeta.Namespace).Update(context.TODO(), &pvc, metav1.UpdateOptions{})
 	if err != nil {
 		return err
 	}
-	log.Printf("DEBUG: UpdatePvc result:  %+v\n", result)
 	return nil
 }
 
 func GetNode(nodeName string, kubeClient kubernetes.Interface) (*v1.Node, error) {
-	node, err := kubeClient.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
+	node, err := kubeClient.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
  	if err != nil {
 		return nil, err
 	}
@@ -67,10 +64,9 @@ func GetNode(nodeName string, kubeClient kubernetes.Interface) (*v1.Node, error)
 }
 
 func UpdateNodeStatus(nodeName string, kubeClient kubernetes.Interface, node *v1.Node) error {
-	updatenode, err := kubeClient.CoreV1().Nodes().UpdateStatus(node)
+	_, err := kubeClient.CoreV1().Nodes().UpdateStatus(context.TODO(), node, metav1.UpdateOptions{})
 	if err != nil {
 		return err
 	}
-	log.Printf("DEBUG: UpdateNodeStatus: %+v\n", updatenode)
 	return nil
 }
